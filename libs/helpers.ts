bec7f16bb6fa30fe4737a0e87cedc0ba7226c65c
planetarybeing <awatkis00@icloud.com>
2023-08-06 15:40:09 -0400

    dependency

---
+++ libs/helpers.ts
@@ -0,0 +1,12 @@
+import Stripe from "Stripe";
+
+export const stripe = new Stripe(
+    process.env.STRIPE_SECRET_KEY,
+    {
+        apiVersion: '2022-11-15',
+        appInfo: {
+            name: 'Spotify Clone Video',
+            
+        }
+    }
+)
