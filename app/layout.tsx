bc8ac1c2dffe9041eacc3a7105b9774174d4b31a
planetarybeing <awatkis00@icloud.com>
2023-08-08 13:16:09 -0400

    API update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,47 @@
-import SideBar from '@/components/Sidebar'
 import { Figtree } from 'next/font/google'
+
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
+import Sidebar from '@/components/Sidebar'
+import ToasterProvider from '@/providers/ToasterProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import Player from '@/components/Player'
+
+import './globals.css'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify-Clone',
-  description: 'Music discovery app',
+  title: 'Spotify Clone',
+  description: 'Spotify Clone',
 }
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const products = await getActiveProductsWithPrices();
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-        {children}
-        </Sidebar>
-        </body>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
